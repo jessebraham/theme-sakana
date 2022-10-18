451a400cfa74493e4119e74fc7af03bd5df981af
Jesse Braham <jesse@beta7.io>
2022-10-17 21:37:47 -0700

    Improve the git branch status display, better Rust integration

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -20,45 +20,58 @@ end
 
 
 function __sakana_git_branch_state
-  echo -n "$dkgrey"
-  echo -n "["
+  # Create an empty array to store our icons
+  set -l icons ()
 
   # Check for any untracked files
   set -l git_untracked (command git ls-files --others --exclude-standard 2> /dev/null)
   if [ -n "$git_untracked" ]
-    echo -n "$green+$normal"
+    set -a icons "$green+$normal"
   end
 
   # Check if there are any local changes to be commited
   if git_is_touched
-    echo -n "$gold!$normal"
+    set -a icons "$gold!$normal"
   else
-    echo -n "$green✔$normal"
+    set -a icons "$green✔$normal"
   end
 
   # Check for any stashed files
   if git_is_stashed
-    echo -n "$blue⚑$normal"
+    set -a icons "$blue⚑$normal"
   end
 
-  echo -n "$dkgrey]$normal"
+  # Print each icon with appropriate padding
+  echo -n "$ltgrey"
+  echo -n "["
+
+  for icon in $icons
+    echo -n " $icon"
+  end
+  echo -n " "
+
+  echo -n "$ltgrey]$dkgrey"
 
   # Check if the branch is ahead, behind, or diverged of remote
-  set -l commit_count (command git rev-list --count --left-right "@{upstream}...HEAD" 2> /dev/null)
-  switch "$commit_count"
+  set -l commits (command git rev-list --count --left-right "@{upstream}...HEAD" 2> /dev/null)
+  switch "$commits"
   case ""
     # No upstream
   case "0"\t"0"
-    # Even
+    # Even with upstream
   case "*"\t"0"
-    set -l count (echo $commit_count | awk '{print $1}')
-    echo " $count commits behind"
+    set -l count (echo $commits | awk '{print $1}')
+    echo -n " $count commits behind"
   case "0"\t"*"
-    set -l count (echo $commit_count | awk '{print $2}')
-    echo " $count commits ahead"
+    set -l count (echo $commits | awk '{print $2}')
+    echo -n " $count commits ahead"
   case "*"
-    echo " DIVERGED"
+    set -l behind (echo $commits | awk '{print $1}')
+    set -l ahead  (echo $commits | awk '{print $2}')
+    echo -n " $behind commit behind, $ahead ahead"
   end
+
+  echo
 end
 
 function __sakana_git_state
@@ -68,7 +81,7 @@ function __sakana_git_state
     #git update-index --really-refresh -q 1> /dev/null
 
     set -l git_branch (command git symbolic-ref --quiet --short HEAD 2> /dev/null; or git rev-parse --short=7 HEAD 2> /dev/null; or echo -n '(???)')
-    echo -n " $red git$dkgrey:$blue$git_branch$normal "
+    echo -n "$red git$dkgrey:$blue$git_branch$normal "
 
     __sakana_git_branch_state
   else
@@ -87,8 +100,12 @@ function fish_prompt
 
   __sakana_git_state
 
+  # Rust integration
   if test -e "Cargo.toml"
-    printf "🦀 $dkgrey%s$normal " (rustc --version | awk '{print $2}')
+    set -l rustc   (rustc --version | awk '{print $2}' | cut -d "-" -f1)
+    set -l toolchain (rustup show | tail -n 3 | head -n 1 |  cut -d '-' -f 1 | cut -d ' ' -f 1)
+
+    echo -n "🦀 $orange$toolchain$dkgrey@$ltgrey$rustc "
   end
 
   set -l lambda "λ"
